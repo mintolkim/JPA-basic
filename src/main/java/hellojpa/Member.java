eddbf19d6f3c176306b51e8eba6ea5e0bdcef4ee
mintol <juna216@naver.com>
2023-05-17 22:42:59 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,35 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 // jpa를 사용하는 애구나 라고 인식
-
 public class Member {
-    @Id // PK를 알려줘야함
+    @Id
     private long id;
-    private String name;
+    @Column(name = "name") // DB 컬럼명은 name
+    private String username;
+    private Integer age; // DB에도 적절한 숫자 타입 선택됨
+
+    // 객체에서 enum 타입을 쓰고싶다면
+    // DB에는 enum 타입이 없다.
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // 날짜 타입을 쓰고 싶다면
+    // 타입은 3가지가 있다. (데이터베이스에는 구분되있기때문에)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 큰 데이터
+    @Lob
+    private String description;
 
     public Member() {
     }
 
-    public Member(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
