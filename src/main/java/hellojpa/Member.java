8b089360e0cb2b0f550b681523da9791861eceef
mintol <juna216@naver.com>
2023-05-18 21:47:20 +0900

    jpa 연간관계 기초

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+)
+// jpa를 사용하는 애구나 라고 인식
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
+    @Column(name = "name") // DB 컬럼명은 name
+    private String username;
+
+    public Member() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/hellojpa/jpaMain.java
@@ -0,0 +1,43 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaMain {
+    public static void main(String[] args){
+        // 애플리케이션 로딩시점에 딱 하나만 만들어야함
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // entitiyManager가 꺼내지는지 확인
+        // 디비에 저장하는 트랜잭션 단위마다는 Manager를 생성해야함
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        // 디비를 실행하는 데 있어서는 트랜잭션이란 단위가 중요 꼭 시작하여야 에러가 나지 않음
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeamId(team.getId());
+            em.persist(member);
+
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = em.find(Team.class, team.getId());
+            tx.commit();
+        } catch (Exception e){
+            // 에러시 롤백
+            tx.rollback();
+        } finally {
+            // entityManager 종료 또한 중요하다.
+            em.close();
+        }
+
+        emf.close();
+    }
+}
