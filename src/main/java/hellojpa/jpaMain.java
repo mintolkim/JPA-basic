445e2cbb66889779f4e277eef0c2e2fcc465c8f0
mintolkim <juna216@naver.com>
2023-05-19 00:03:42 +0900

    push

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -14,29 +14,25 @@ public class jpaMain {
         tx.begin();
 
         try {
-
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team); 
+            member.changeTeam(team);
             em.persist(member);
 
-            // DB 에서 쿼리를 보고 싶다 test 할려면
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
 
             for (Member m : members){
-                System.out.println("m = "+ m.getUsername());
+                System.out.println("m = " + m.getUsername());
             }
-
-            Team newTeam = em.find(Team.class, 100L);
-            findMember.setTeam(newTeam);
 
             tx.commit();
         } catch (Exception e){
